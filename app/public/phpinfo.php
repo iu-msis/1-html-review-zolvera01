81ed2f2a63366117c0dbc5e9ea6cc6ab358bd16b
zolvera01 <89401040+zolvera01@users.noreply.github.com>
2021-09-28 15:31:51 -0400

    Adding PHP files. Also to the GA grading my assignment 3, Can i have this commit graded instead? I believe I may get a few more points for this commit as the other one turned in before the deadline did not actually meet the requirements. If not, then that is okay too. Thank you in advance

---
+++ app/public/phpinfo.php
@@ -0,0 +1,8 @@
+<?php 
+
+phpinfo();
+
+
+//application server and language are both php
+//don't put closing ? and > because any space after closing tag will ruin application
+//instead leave blank as better practice
